837b6c5a04d5ae764580b915317ff157aa51a6e5
Syncxv <aruldevarackal1@gmail.com>
2022-05-16 03:12:31 +0530

    folder structure gang

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,3 +1,19 @@
+import express from 'express'
+import 'reflect-metadata'
 import { initializeRouters } from './routes'
+import database from './connection'
+const port = 3000
 
-export default initializeRouters
+const apiV1Main = async () => {
+    const app = express()
+    await database.initalize()
+    initializeRouters(app)
+    app.get('/', (_, res) => {
+        res.send({ hello: 'wolrd' })
+    })
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`)
+    })
+}
+
+export default apiV1Main
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
-import express from 'express'
-import 'reflect-metadata'
-import initializeRouters from './api/v1'
-import database from './connection'
-const port = 3000
+import apiV1Main from './api/v1'
 
-const main = async () => {
-    const app = express()
-    await database.initalize()
-    initializeRouters(app)
-    app.get('/', (_, res) => {
-        res.send({ hello: 'wolrd' })
-    })
-    app.listen(port, () => {
-        console.log(`listening on port ${port}`)
-    })
-}
-
-main().catch(err => console.error(err))
+apiV1Main().catch(err => console.error(err))
