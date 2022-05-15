d6bd68dafaeda6d292fb82bdac9b999c5c2aedfe
Syncxv <aruldevarackal1@gmail.com>
2022-05-16 03:14:54 +0530

    DATABASE class works :D
    WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- src/api/v1/controllers/users.ts
+++ src/api/v1/controllers/users.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express'
+import database from '../connection'
+import { User } from '../entities/User'
 
-export const getUsers = (_req: Request, res: Response) => {
-    res.send({ users: [] })
+export const getUsers = async (_req: Request, res: Response) => {
+    res.send({ users: await database.connection.manager.find(User) })
 }
